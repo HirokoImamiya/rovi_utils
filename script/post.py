d60b06e32f654d717830f9374d8c5ff9bb2fd9df
HirokoImamiya <imamiya@yoods.co.jp>
2023-02-09 10:57:29 +0900

    位相あわせのマスタの中心軸検出対応

--- script/post.py
+++ script/post.py
@@ -20,6 +20,7 @@ from geometry_msgs.msg import Transform
 from geometry_msgs.msg import TransformStamped
 from rovi_utils import tflib
 from rovi_utils.srv import TextFilter
+from rovi_utils import sym_solver as rotsym
 
 Param={
   "evaluate":False,
@@ -27,7 +28,9 @@ Param={
   "var":[],
   "pitch_subd":0,
   "repeat":1,
-  "eval_threshold":0
+  "eval_threshold":0,
+  "icp_threshold":0,
+  "rotate":0
 }
 Config={
   "proc":0,
@@ -70,13 +73,34 @@ def learn_feat(mod,param):
   if Config["proc"]==0: o3d.io.write_point_cloud("/tmp/sub_model.ply",pcd[0])
   return pcd
 
+def learn_rot(pc,num,thres):
+  global RotAxis,tfReg
+  RotAxis=None
+  if num>1 and (not pc.is_empty()):
+    RotAxis=rotsym.solve(pc,num,thres)
+    if len(RotAxis)>1:
+      tf=TransformStamped()
+      tf.header.stamp=rospy.Time.now()
+      tf.header.frame_id=Config["master_frame_ids"][0]
+      tf.child_frame_id=Config["master_frame_ids"][0]+'/axis'
+      tf.transform=tflib.fromRT(RotAxis[0])
+      tfReg.append(tf)
+    else:
+      RotAxis=None
+      print('No axis')
+      pub_err.publish("searcher::No axis")
+
+def solve_rot(tr):
+  global EvalScore,MsgScore
+  for key in EvalScore: EvalScore[key]=[]
+  MsgScore=list()
+  return do_eval_solve(tr,rot=True)
+
 def isEvaluate():
   return Param['evaluate']
 
-def get_solve_result():
+def get_solve_result(source, target):
   ts=Transform()
-  source=Config['master_main_frame_id']
-  target=Config['solve_frame_id']+'/solve0'
   if tf_lookup is not None:
     req='base '+source+ ' '+target
     try:
@@ -89,9 +113,28 @@ def get_solve_result():
     print("tf_lookup None")
   return ts
 
+def change_main_result():
+  base=Config['base_frame_id']
+  ts=get_solve_result(Config['master_main_frame_id'], Config['solve_frame_id']+'/solve0')
+  bTc=getRT('base',Config["master_frame_ids"][0])
+  cTb=np.linalg.inv(bTc)
+  cTc=tflib.toRT(ts)
+  Tm=cTb.dot(cTc).dot(bTc)
+  wTc=getRT(Config["solve_frame_id"],Config["master_frame_ids"][0])
+  tr=tflib.fromRT(wTc.dot(Tm))
+  set_solve_sub_pos(tr)
+  return tr
+
 def set_axis_pos():
   base=Config['base_frame_id']
-  target=Config['master_main_frame_id']+'/axis'
+  if RotAxis is not None:
+    target=Config["master_frame_ids"][0]+'/axis'
+    solve_source=Config["master_frame_ids"][0]
+    solve_target=Config['solve_frame_id']+'/solve0_sub'
+  else:
+    target=Config['master_main_frame_id']+'/axis'
+    solve_source=Config['master_main_frame_id']
+    solve_target=Config['solve_frame_id']+'/solve0'
   axis_master=Config['axis_frame_id']+'/master0'
   axis_solve=Config['axis_frame_id']+'/solve0'
   wTc=getRT(base,target)
@@ -105,7 +148,7 @@ def set_axis_pos():
     tf.transform=tflib.fromRT(wTc)
     broadcaster.sendTransform(tf)
 
-    ts=get_solve_result()
+    ts=get_solve_result(solve_source,solve_target)
     bTc=getRT('base',axis_master)
     cTb=np.linalg.inv(bTc)
     cTc=tflib.toRT(ts)
@@ -151,7 +194,7 @@ def set_score(score):
   EvalScore["transform"].append(ts)
 
 def cb_done(result,finish=False):
-  global isExec
+  global isExec,T1
   if finish:
     tsolve=time.time()-T1
     stats={}
@@ -166,12 +209,14 @@ def cb_score(n,finish):
   if finish:
     pub_score.publish(MsgScore[n])
   set_solve_sub_pos(EvalScore["transform"][n])
+  if Config['debugoutput']:
+    rospy.Timer(rospy.Duration(0.1),cb_master,oneshot=True)
 
 def cb_master(event):
   if Config["proc"]==0:
     for n,l in enumerate(Config["scenes"]):
       if Model[n] is not None:
-        print("publish master",len(Model[n]))
+        print("post::publish master",len(Model[n]))
         pub_pcs[n].publish(np2F(Model[n]))
 
 def cb_save(msg):
@@ -210,6 +255,7 @@ def cb_save(msg):
     tfReg.append(tf)
   if Config["proc"]==0: broadcaster.sendTransform(tfReg)
   pcd=learn_feat(Model,Param)
+  learn_rot(pcd[0],Param['rotate'],Param['threshold'])
   pub_msg.publish("post::master plys and frames saved")
   pub_saved.publish(mTrue)
   rospy.Timer(rospy.Duration(0.1),cb_master,oneshot=True)
@@ -223,7 +269,6 @@ def cb_load(msg):
       Model[n]=np.reshape(np.asarray(pcd.points),(-1,3))
     else:
       Model[n]=[]
-  rospy.Timer(rospy.Duration(0.1),cb_master,oneshot=True)
   tfReg=[]
 #load TF such as master/camera...
   for m in Config["master_frame_ids"]:
@@ -249,8 +294,10 @@ def cb_load(msg):
       tf.transform=trf
       tfReg.append(tf)
       Param['transform']=tflib.toRT(trf)
+  rospy.Timer(rospy.Duration(0.1),cb_master,oneshot=True)
   update_param()
   pcd=learn_feat(Model,Param)
+  learn_rot(pcd[0],Param['rotate'],Param['threshold'])
   if Config["proc"]==0: broadcaster.sendTransform(tfReg)
   pub_msg.publish("post::model loaded and learning completed")
   pub_loaded.publish(mTrue)
@@ -274,7 +321,6 @@ def cb_main_judged(msg):
         pub_msg.publish("post::Lacked scene to solve")
         cb_done(False)
       else:
-        set_axis_pos()
         print("cb_solve_do start")
         rospy.Timer(rospy.Duration(0.01),cb_solve_do,oneshot=True)
     else:
@@ -288,9 +334,14 @@ def cb_sub_judged(msg):
   cb_done(msg.data,True)
   print("post solve sub end")
 
-def do_eval_solve():
+def do_eval_solve(tr,rot=False):
   score={"proc":[],"Tx":[],"Ty":[],"Tz":[],"Qx":[],"Qy":[],"Qz":[],"Qw":[]}
-  result=solver.solve(Scene,Param)
+  prm=Param.copy()
+  prm['tf']=tflib.tf2dict(tr)
+  if rot:
+    prm["eval_threshold"]=0
+    prm["icp_threshold"]=prm["threshold"]
+  result=solver.solve(Scene,prm)
   RTs=result["transform"]
   if np.all(RTs[0]):
     pub_err.publish("post::solver error")
@@ -298,9 +349,33 @@ def do_eval_solve():
   else:
     pub_msg.publish("post::"+str(len(RTs))+" model searched")
 
+  if rot:
+    cTb=getRT('camera/capture0','base')
+    xvec=cTb[:3,:3].T[0]
   for n,rt in enumerate(RTs):
     print('Post',tflib.fromRT(rt))
-    tf=tflib.fromRT(rt)
+    if rot:
+      if RotAxis is not None:
+        wrt=[]
+        rot=[]
+        for m,wt in enumerate(RotAxis): #to minimumize the rotation
+          if m==0: wrt.append(rt)
+          else: wrt.append(np.dot(rt,wt))
+          R=wrt[n][:3,:3]
+          rot.append(np.inner(R.T[0],xvec))
+        tf=tflib.fromRT(wrt[np.argmin(np.array(rot))])
+        print('TF axis 1',tf)
+        solver.score["transform"][n]=tflib.toRT(tf)
+        prm=Param.copy()
+        prm["distance_threshold"]=0
+        prm["repeat"]=1
+        result=solver.solve(Scene,prm)
+        tf=tflib.fromRT(result["transform"][0])
+        print('TF axis 2',tf)
+      else:
+        tf=tflib.fromRT(rt)
+    else:
+     tf=tflib.fromRT(rt)
 
     score["Tx"].append(tf.translation.x)
     score["Ty"].append(tf.translation.y)
@@ -326,7 +401,7 @@ def rotz_eval_solve(wTc,bTu,vars):
   result=False
   if wTc is not None:
     for n,rz in enumerate(vars):
-      rospy.loginfo("post::eval solve n=%d degree=%d",n,rz)
+      rospy.loginfo("post::eval solve n=%d degree=%.2f",n,rz)
       rt=np.eye(4)
       rt[:3,:3]=R.from_euler('z',rz,degrees = True).as_matrix()
       wTcc=rt.dot(wTc)
@@ -336,12 +411,11 @@ def rotz_eval_solve(wTc,bTu,vars):
       Tm=np.eye(4)
       Tm[:3,:3]=rot.as_matrix()
       Tm[:3,3]=np.array(pos[:3]).T
-      Param['tf']=tflib.tf2dict(tflib.fromRT(bTu.dot(Tm)))
-      result=do_eval_solve()
+      result=do_eval_solve(tflib.fromRT(bTu.dot(Tm)))
       if result is False:
         break
       if Config['debugoutput']:
-        rospy.loginfo("post::eval score fitness=%.2f rmse=%.2f",EvalScore["fitness"][n],EvalScore["rmse"][n])
+        rospy.loginfo("post::eval score n=%d fitness=%.5f rmse=%.5f",n,EvalScore["fitness"][n],EvalScore["rmse"][n])
         cb_score(n,False)
         stats={}
         stats['angle_temp']=rz
@@ -351,11 +425,22 @@ def rotz_eval_solve(wTc,bTu,vars):
   return result
 
 def cb_solve_do(msg):
+  tr=change_main_result()
+  if RotAxis is not None:
+    if solve_rot(tr):
+      cb_score(0,False)
+      rospy.sleep(0.01)
+    else:
+      cb_done(False)
+      return
+  set_axis_pos()
+  rospy.sleep(0.01)
+
   if Param['pitch']: vars=np.arange(Param['var'][0],Param['var'][1],Param['pitch'])
   else: vars=Param['var']
   uf=Config['axis_frame_id'] + '/solve0'
   source=Config['solve_frame_id']
-  target=Config['solve_frame_id']+'/solve0'
+  target=Config['solve_frame_id']+'/solve0_sub'
   wTc=getRT(uf, target)
   bTu=getRT(source, uf)
   result=rotz_eval_solve(wTc,bTu,vars)
@@ -368,16 +453,18 @@ def cb_solve_do(msg):
     if Param['pitch'] and Param['pitch_subd']:
       start=vars[m]-Param['pitch']/2
       end=vars[m]+Param['pitch']/2
-      print("post eval step2 range=", start, ",", end)
-      vars=np.arange(start,end,Param['pitch_subd'])
+      print("post eval step2 m=", m, "range=", start, ",", end)
+      vars=np.arange(start,end,Param['pitch_subd'],float)
       result=rotz_eval_solve(wTc,bTu,vars)
       if result:
         m=np.argmax(EvalScore['fitness'])
+        maxindex=[i for i, x in enumerate(EvalScore['fitness']) if x == max(EvalScore['fitness'])]
+        print("post result max=", maxindex)
         stats['angle_step2']=vars[m]
         stats['fitness_step2']=EvalScore["fitness"][m]
 
   if result:
-    rospy.loginfo("post::eval score fix angle=%d",vars[m])
+    rospy.loginfo("post::eval score fix angle=%.3f num=%d",vars[m],m)
     cb_score(m,True)
     stats['angle']=vars[m]
   else:
@@ -441,6 +528,9 @@ def update_param():
   except Exception as e:
 #    print("get_param exception:",e.args)
     pass
+  Param['threshold']=Param['eval_threshold']
+  if Param['icp_threshold']:
+    Param['threshold']=Param['icp_threshold']
 
 def parse_argv(argv):
   args={}
@@ -514,6 +604,7 @@ broadcaster=tf2_ros.StaticTransformBroadcaster()
 ###data
 Scene=[None]*len(Config["scenes"])
 Model=[None]*len(Config["scenes"])
+RotAxis=None
 tfReg=[]
 MsgScore=list()
 isExec=False
--- src/rovi_utils/evaluate_solver.py
+++ src/rovi_utils/evaluate_solver.py
@@ -7,6 +7,9 @@ import time
 from rovi_utils import tflib
 
 Param={
+  "normal_radius":0.01,
+  "normal_min_nn":0,
+  "icp_threshold":0.003,
   "eval_threshold":0
 }
 
@@ -22,6 +25,22 @@ def fromNumpy(dat):
   pc.points=o3d.utility.Vector3dVector(dat)
   return pc
 
+def _get_features(cloud):
+  o3d.geometry.PointCloud.estimate_normals(cloud,o3d.geometry.KDTreeSearchParamRadius(radius=Param["normal_radius"]))
+  viewpoint=np.array([0.0,0.0,0.0],dtype=float)
+  o3d.geometry.PointCloud.orient_normals_towards_camera_location(cloud, camera_location=viewpoint)
+  nfmin=Param["normal_min_nn"]
+  if nfmin<=0: nfmin=1
+  cl,ind=o3d.geometry.PointCloud.remove_radius_outlier(cloud,nb_points=nfmin,radius=Param["normal_radius"])
+  nfcl=o3d.geometry.PointCloud.select_by_index(cloud,ind)
+  cloud.points=nfcl.points
+  cloud.normals=nfcl.normals
+  cds=cloud
+#  if Param["feature_mesh"]>0:
+#    cds=o3d.geometry.PointCloud.voxel_down_sample(cloud,voxel_size=Param["feature_mesh"])
+#  return cds,o3d.pipelines.registration.compute_fpfh_feature(cds,o3d.geometry.KDTreeSearchParamRadius(radius=Param["feature_radius"]))
+  return cds
+
 def learn(datArray,prm):
   global modPcArray,Param
   Param.update(prm)
@@ -29,6 +48,8 @@ def learn(datArray,prm):
   for dat in datArray:
     pc=fromNumpy(dat)
     modPcArray.append(pc)
+    if not pc.is_empty():
+      _get_features(pc)
   return modPcArray
 
 def solve(datArray,prm):
@@ -39,6 +60,8 @@ def solve(datArray,prm):
   for dat in datArray:
     pc=fromNumpy(dat)
     scnPcArray.append(pc)
+    if not pc.is_empty():
+      _get_features(pc)
   tfeat=time.time()-t1
   print("evaluate solver::time for calc feature",tfeat)
   t1=time.time()
@@ -50,10 +73,19 @@ def solve(datArray,prm):
     n=Param["repeat"]
     score={"transform":[RT]*n,"fitness":[None]*n,"rmse":[None]*n}
   for n in range(Param["repeat"]):
-    result=o3d.pipelines.registration.evaluate_registration(modPcArray[0],scnPcArray[0],Param["eval_threshold"],RT)
     score["transform"][n]=RT
-    score["fitness"][n]=result.fitness
-    score["rmse"][n]=result.inlier_rmse
+    if Param["icp_threshold"]>0:
+      result=o3d.pipelines.registration.registration_icp(
+        modPcArray[0],scnPcArray[0],
+        Param["icp_threshold"],
+        score["transform"][n],o3d.pipelines.registration.TransformationEstimationPointToPlane())
+      score["transform"][n]=result.transformation
+      score["fitness"][n]=result.fitness
+      score["rmse"][n]=result.inlier_rmse
+    if Param["eval_threshold"]>0:
+      result=o3d.pipelines.registration.evaluate_registration(modPcArray[0],scnPcArray[0],Param["eval_threshold"],score["transform"][n])
+      score["fitness"][n]=result.fitness
+      score["rmse"][n]=result.inlier_rmse
   tmatch=time.time()-t1
   print("evaluate solver::time for feature matching",tmatch)
   score["tfeat"]=tfeat
